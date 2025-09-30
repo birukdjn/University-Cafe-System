32a5d7a064f8e63e9d8e8b092494c95cee3854b2
Biruk Dejene <birukedjn@gmail.com>
2025-09-30 18:05:45 +0300

    done

--- backend/build.sh
+++ backend/build.sh
@@ -10,10 +10,3 @@ python manage.py collectstatic --no-input
 
 # Apply database migrations
 python manage.py migrate
-
-if [ -n "$CREATE_SUPERUSER" ]; then
-    echo "Creating superuser..."
-    # The --noinput flag tells Django to use the DJANGO_SUPERUSER_... environment variables
-    python manage.py createsuperuser --noinput
-    echo "Superuser creation command executed."
-fi
